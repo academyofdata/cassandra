da59818a56b61b455c39677789af3a26eba0a745
felix <felix@netopia.ro>
2016-07-11 10:40:33 +0300

    Added async execute example

--- src/main/java/com/academyofdata/cj/CassJ.java
+++ src/main/java/com/academyofdata/cj/CassJ.java
@@ -6,6 +6,7 @@ import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.ResultSetFuture;
 import com.datastax.driver.core.Row;
 /**
  * Created by felix on 28/06/16.
@@ -26,10 +27,30 @@ public class CassJ {
     }
 
     public void getData(){
-        ResultSet results = session.execute("SELECT * FROM metro.data");
+        ResultSet results = session.execute("SELECT * FROM data.users");
+        int count = 0;
         for (Row row : results) {
-            System.out.println(String.format("%s\t%s\t%d", row.getUUID("idx").toString(), row.getString("field1"),  row.getInt("field2")));
+            System.out.println(String.format("%d:%s\t%s\t%s", ++count,row.getUUID("uid").toString(), row.getString("gender"),  row.getString("zip")));
         }
+    }
+
+    public void getAsyncData(){
+        ResultSetFuture future = session.executeAsync("SELECT * FROM data.users");
+
+        while (!future.isDone()) {
+            System.out.println("Waiting for request to complete");
+        }
+
+        try {
+            ResultSet rs = future.get();
+            int count = 0;
+            for(Row row : rs) {
+                System.out.println(String.format("%d:%s\t%s\t%s", ++count,row.getUUID("uid").toString(), row.getString("gender"),  row.getString("zip")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void close() {
@@ -40,7 +61,8 @@ public class CassJ {
     public static void main(String[] args) {
         CassJ client = new CassJ();
         client.connect("192.168.56.88");
-        client.getData();
+        //client.getData();
+        client.getAsyncData();
         client.close();
     }
 
